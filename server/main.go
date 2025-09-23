36246b2dc49f6f6613feb62ac9221358ac38392e
David Chau <davidc2509@gmail.com>
2025-09-23 14:09:38 +0200

    Added routes get /health and Get /flights

--- server/main.go
+++ server/main.go
@@ -5,8 +5,19 @@ package main
 
 import (
 	"fmt"
+	"net/http"
 )
 
 func main() {
 	fmt.Println("starting point !")
+
+	server := http.NewServeMux()
+
+	server.HandleFunc("GET /health", func(responseWriter http.ResponseWriter, request *http.Request) {
+		fmt.Fprint(responseWriter, "got path\n")
+	})
+	server.HandleFunc("GET /flights", func(responseWriter http.ResponseWriter, request *http.Request) {
+		fmt.Fprintf(responseWriter, "handling flights request")
+	})
+	http.ListenAndServe("localhost:3008", server)
 }
